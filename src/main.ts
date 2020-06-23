48c20f8a9a5373621222ca1dc55f69497c8d5c63
see7788 <kitchens@qq.com>
2020-06-24 05:19:22 +0800

    newinit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
+import App from "./vue/main.vue";
+import VueRouter from "vue-router";
+import Vuex from "vuex";
+Vue.use(VueRouter);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
-
+const store = new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+});
+const router = new VueRouter({
+  // routes: [
+  //   {
+  //     path: "/",
+  //     name: "Home",
+  //     component: () => import(/* webpackChunkName: "about" */ "../views/Home.vue")
+  //   },
+  //   {
+  //     path: "/about",
+  //     name: "About",
+  //     // route level code-splitting
+  //     // this generates a separate chunk (about.[hash].js) for this route
+  //     // which is lazy-loaded when the route is visited.
+  //     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  //   }
+  // ]
+});
 new Vue({
   router,
   store,
